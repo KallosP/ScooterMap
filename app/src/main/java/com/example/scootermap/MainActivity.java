382e94ca50938f7c7b5654cd204a13f170fed65a
KallosP <97070073+KallosP@users.noreply.github.com>
2023-07-28 12:00:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/scootermap/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scootermap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // Program entry point
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
